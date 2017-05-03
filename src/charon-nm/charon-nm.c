gityf/strongswan

commit af16b5afb0ee93f4706058006d45b847fdd556a2

Use random ports in NetworkManager backend

src/charon-nm/charon-nm.c
@@ -206,6 +206,10 @@ int main(int argc, char *argv[])
 
 	initialize_logger();
 
+	/* use random ports to avoid conflicts with regular charon */
+	lib->settings->set_int(lib->settings, "charon-nm.port", 0);
+	lib->settings->set_int(lib->settings, "charon-nm.port_natt_t", 0);
+
 	DBG1(DBG_DMN, "Starting charon NetworkManager backend (strongSwan "VERSION")");
 	if (lib->integrity)
 	{

commit c1cc89d8adde6c726a8f796da7269334f9e745fc

charon-nm: Fix typo to actually use random NAT-T port
Fixes: af16b5afb0ee ("Use random ports in NetworkManager backend")

src/charon-nm/charon-nm.c
@@ -196,7 +196,7 @@ int main(int argc, char *argv[])
 
 	/* use random ports to avoid conflicts with regular charon */
 	lib->settings->set_int(lib->settings, "charon-nm.port", 0);
-	lib->settings->set_int(lib->settings, "charon-nm.port_natt_t", 0);
+	lib->settings->set_int(lib->settings, "charon-nm.port_nat_t", 0);
 
 	DBG1(DBG_DMN, "Starting charon NetworkManager backend (strongSwan "VERSION")");
 	if (lib->integrity)